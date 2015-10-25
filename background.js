dde92680e488fdf9a4061c736d2c84265e83d4a5
Jay Ravaliya <jayrav13@gmail.com>
2015-10-25 03:20:56 -0400

    Everything works with Mandrill

--- background.js
+++ background.js
@@ -10,6 +10,7 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 	if (changeInfo.status == 'complete') {   
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
 			var activeTab = tabs[0]
+
 			if(activeTab.url.indexOf("q=notepad") != -1) {
 				chrome.tabs.sendMessage(tabs[0].id, {message: "notepad"}, function(response) {});
 			}
--- content.js
+++ content.js
@@ -1,13 +1,3 @@
-$(document).ready(function() {
-	console.log("From content!");
-	if(window.location.href.indexOf("q=notepad") != -1) {
-		console.log("A google site!");
-	}
-	else {
-		console.log("Not found!");
-	}
-});
-
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 		if(request.message != "notepad") {
@@ -15,9 +5,49 @@ chrome.runtime.onMessage.addListener(
 		}
 		else {
 			console.log("Message successfully arrives!");
-			$("#resultStats").html("<textarea style=\"position:fixed\" rows=\"3\" cols=\"80\" placeholder=\"Welcome to your Google Notepad!\"></textarea>");
+			$("#resultStats").html("<textarea rows=\"3\" cols=\"80\"  id=\"jr-notepad\" class=\"jr-notepad\" placeholder=\"Welcome to your Google Notepad!\"></textarea>");
+			$("#jr-notepad").on("keyup", function(e) {
+				if(e.which == 13 && !e.shiftKey) {
+					console.log($("#jr-notepad").val());
+					test();
+				}
+			});
+
+
 		}
 	}
 );
 
+function test() {
 
+	var email = $(".gb_8a").text();
+	var message = $("#jr-notepad").val();
+
+	data = {
+		email:email,
+		message:message
+	}
+	$.ajax({
+		type: 'POST',
+		url: 'https://mandrillapp.com/api/1.0/messages/send.json',
+		data: {
+			'key': "2i_R3FcCFGjIQYmH5dsuVw",
+			'message': {
+				'from_email': 'jayrav13@gmail.com',
+				'to': [
+					{
+						'email': email,
+						'type': 'to'
+					}
+				],
+				'autotext': 'true',
+				'subject': 'New note from Google Notes!',
+				'html': message 
+			}
+		}
+	}).success(function(response) {
+		alert("Sent a message to " + email + "!");
+		$("#jr-notepad").val("");
+		console.log(response); // if you're into that sorta thing
+	});
+}
